bde672c7045bbfe4c006aff8568b9bfc552a3e39
Brian Steere <dianoga7@3dgo.net>
2014-11-11 07:08:02 -0600

    Update whole-house-fan.sa.groovy

--- whole-house-fan.sa.groovy
+++ whole-house-fan.sa.groovy
@@ -19,8 +19,8 @@ definition(
     author: "Brian Steere",
     description: "Toggle a whole house fan (switch) when: Outside is cooler than inside, Inside is above x temp, Thermostat is off",
     category: "Green Living",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Developers/whole-house-fan.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Developers/whole-house-fan%402x.png"
 )
 
 
